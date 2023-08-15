58c03105ccc3100a02b3894ab4e9093bc9b5d0a2
guilhermecoelho2 <guilhermecoelho2@gmail.com>
2023-08-15 19:39:14 +0100

    include AsSplitQuery on searches

--- RinhaBackend/Data/PessoaData.cs
+++ RinhaBackend/Data/PessoaData.cs
@@ -21,14 +21,14 @@ namespace RinhaBackend.Data
         }
 
         public async Task<PessoasModel> GetById(Guid id)
-            => await _db.Pessoas.Include(x => x.Stacks).FirstOrDefaultAsync(x => x.Id == id);
+            => await _db.Pessoas.Include(x => x.Stacks).AsSplitQuery().FirstOrDefaultAsync(x => x.Id == id);
 
         public async Task<bool> IsApelidoExist(string apelido)
-           => await _db.Pessoas.AnyAsync(x => x.Apelido == apelido);
+           => await _db.Pessoas.AsSplitQuery().AnyAsync(x => x.Apelido == apelido);
 
         public async Task<IQueryable<PessoasModel>> SearchByString(string search)
         {
-            return _db.Pessoas.Include(x => x.Stacks).Take(50).Where(x =>
+            return _db.Pessoas.Include(x => x.Stacks).AsSplitQuery().Take(50).Where(x =>
             (EF.Functions.Like(x.Nome, $"%{search}%"))
             || (EF.Functions.Like(x.Apelido, $"%{search}%"))
             || (x.Stacks.Any(c => (EF.Functions.Like(c.Nome, $"%{search}%")))));
