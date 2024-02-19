f2d9d2fe478999a75c2492a7d4c2049090865612
guilhermecoelho2 <guilhermecoelho2@gmail.com>
2024-02-19 21:16:15 +0000

    inject services

---
+++ RinhaBackendV2/ExceptionHandleMiddleware.cs
@@ -0,0 +1,42 @@
+﻿namespace RinhaBackendV2
+{
+    public class ExceptionHandleMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public ExceptionHandleMiddleware(RequestDelegate next)
+        {
+            _next= next;
+        }
+
+        public async Task Invoke(HttpContext httpContext)
+        {
+            try
+            {
+                await _next(httpContext);
+            }catch(Exception e)
+            {
+                await HandleException(e, httpContext);
+            }
+        }
+
+        private async Task HandleException(Exception ex, HttpContext httpContext)
+        {
+
+
+            if (ex is InvalidOperationException)
+            {
+                if(ex.Message == "cliente nao existe")
+                    Results.NotFound(ex);
+            }
+        }
+    }
+
+    public static class ExceptionHandleMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseExceptionHandleMiddleware(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<ExceptionHandleMiddleware>();
+        }
+    }
+}
--- RinhaBackendV2/Program.cs
+++ RinhaBackendV2/Program.cs
@@ -1,8 +1,11 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using RinhaBackendV2;
 using RinhaBackendV2.Models;
 using RinhaBackendV2.Models.Requests;
 using RinhaBackendV2.Models.Responses;
+using RinhaBackendV2.Service;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,12 +16,13 @@ builder.Services.AddSwaggerGen();
 
 var dbBuilder = new DbContextOptionsBuilder<RinhaContext>().UseInMemoryDatabase("testing");
 
+
 var db = new RinhaContext(dbBuilder.Options);
 
 db.Cliente.Add(new Cliente
 {
     Id = 1,
-    Nome= "o barato sai caro",
+    Nome = "o barato sai caro",
     Limite = 1000 * 100,
     Saldo = 0
 });
@@ -46,6 +50,9 @@ db.SaveChanges();
 //others
 builder.Services.AddTransient<IHttpContextAccessor, HttpContextAccessor>();
 
+builder.Services.AddSingleton<ExtratoService>(new ExtratoService(db));
+builder.Services.AddSingleton<TransacaoService>(new TransacaoService(db));
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -57,14 +64,36 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+//midlleware exceptions
+//app.UseExceptionHandleMiddleware();
+
+
+
+
+var _extratoService = app.Services.GetRequiredService<ExtratoService>();
+var _transacaoSerice = app.Services.GetRequiredService<TransacaoService>();
+
 //global variables
 
 app.MapPost("/clientes/{id}/transacoes", async ([FromRoute] int id, TransacoesRequest request) =>
 {
+    try
+    {
+        request.ClienteId = id;
+        var result = await _transacaoSerice.Add(request);
 
-    var result = new TransacoesResponse();
+        return Results.Ok(result);
+    }
+    catch (Exception ex)
+    {
+        if (ex.Message == "cliente nao existe")
+            return Results.NotFound();
 
-    return Results.Ok(result);
+        if (ex.Message == "saldo menor que limite")
+            return Results.UnprocessableEntity();
+
+        return Results.NotFound();
+    }
 
 }).Produces<TransacoesResponse>();
 
